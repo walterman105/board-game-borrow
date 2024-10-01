0c490819f5eb45e0ca31d0016dd96569f0304658
Alexander Richter <lexrichter@gmail.com>
2024-10-01 17:35:50 -0500

    Added request

--- client/client.py
+++ client/client.py
@@ -22,10 +22,18 @@ boardGames = {}
 
 def addBoardGame():
     name = input("Enter the name of the board game: ")
+    if name in boardGames:
+        print("Would you like to increase the number of copies of the game? (y/n)")
+        choice = input()
+        if choice == "y":
+            newgamecount = int(input("Enter the number of extra game copies you are adding: "))
+            boardGames[name]["gamecount"] = gamecount + newgamecount
+        return
     playercount = input("Enter the number of players: ")
     gametime = input("Enter the time it takes to play the game: ")
     age = input("Enter the minimum age to play the game: ")
-    boardGames[name] = {"playercount": playercount, "gametime": gametime, "age": age}
+    gamecount = int(input("Enter the number of game copies: "))
+    boardGames[name] = {"playercount": playercount, "gametime": gametime, "age": age, "gamecount": gamecount}
 
 def removeBoardGame():
     name = input("Enter the name of the board game: ")
@@ -38,12 +46,26 @@ def displayBoardGames():
         print("Game time: " + boardGames[game]["gametime"])
         print("Age: " + boardGames[game]["age"])
 
+def requestGame():
+    name = input("Enter the name of the board game: ")
+    if name in boardGames:
+        print("There are " + str(boardGames[name]["gamecount"]) + " copies of the game available")
+        request = input("Would you like to request the game? (y/n)")
+        if request == "y":
+            boardGames[name]["gamecount"] = boardGames[name]["gamecount"] - 1
+            print("Game has been requested")
+        else:
+            print("Game has not been requested")
+    else:
+        print("Game is not available")
+
 while 1:
     print("1. Add")
     print("2. Remove")
     print("3. Display")
+    print("4. Request Game")
     print("0. Exit")
-    option = input()
+    option = input("Enter an option: ")
 
     if option == "1":
         addBoardGame()
@@ -51,6 +73,8 @@ while 1:
         removeBoardGame()
     elif option == "3":
         displayBoardGames()
+    elif option == "4":
+        requestGame()
     elif option == "0":
         exit()
 
