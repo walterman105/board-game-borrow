af68ef12a88dffd2b409667cb1d59ab3092a2ec8
Alexander Richter <lexrichter@gmail.com>
2024-10-08 17:29:53 -0500

    Frame in frame

--- client/gui.py
+++ client/gui.py
@@ -1,6 +1,16 @@
 
 import customtkinter
 
+class LoginFrame(customtkinter.CTkFrame):
+    def __init__(self, master):
+        super().__init__(master)
+
+        button = customtkinter.CTkButton(self, text="Username", command=button_callback)
+        button.grid(row=0, column=0, padx=20, pady=20)
+
+        button = customtkinter.CTkButton(self, text="Password", command=button_callback)
+        button.grid(row=1, column=0, padx=20, pady=20)
+
 class FunctionFrame(customtkinter.CTkFrame):
     def __init__(self, master):
         super().__init__(master)
@@ -26,11 +36,21 @@ class UserFunctions(customtkinter.CTk):
         self.grid_columnconfigure((1, 2), weight=1)
         self.grid_rowconfigure((1, 2), weight=1)
 
-        self.buttonframe = FunctionFrame(self)
-        self.buttonframe.grid(row=1, column=1, padx=10, pady=(10, 0), columnspan=2)
+        self.current_frame = None
+        self.switch_frame(LoginFrame)
+        
+        button = customtkinter.CTkButton(self, text="Switch to Function Frame", command=lambda: self.switch_frame(FunctionFrame))
+        button.grid(row=2, column=1, padx=20, pady=20, sticky="sw")
 
         button = customtkinter.CTkButton(self, text="Exit", command=self.quit)
         button.grid(row=2, column=2, padx=20, pady=20, sticky="es")
+
+    def switch_frame(self, frame_class):
+        new_frame = frame_class(self)
+        if self.current_frame is not None:
+            self.current_frame.destroy()
+        self.current_frame = new_frame
+        self.current_frame.grid(row=1, column=1, padx=10, pady=(10, 0), columnspan=2)
 
 def button_callback():
     print("button pressed")
