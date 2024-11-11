a20900554b18cf38783e6e535e83a55a4da03df5
gp038341 <71278263+gp038341@users.noreply.github.com>
2024-11-11 13:28:23 -0600

    Add files via upload

--- Modify Database.py
+++ Modify Database.py
@@ -17,23 +17,32 @@ insert_query = """
     INSERT INTO boardgames (name, minPlayerCount, maxPlayerCount, gametime, age, gamecount)
     VALUES (%s, %s, %s, %s, %s, %s);
 """
-name = input("Enter the name of the game: ").upper()
-minPlayerCount = int(input("Enter the minimum player count: "))
-maxPlayerCount = int(input("Enter the maximum player count: "))
-time = int(input("Enter the amount of minutes the game usually takes: "))
-age = int(input("Enter the minimum recommended age to play the game: "))
+print("1. Add Game")
+print("2. Display Database")
+choice = input("Enter your choice: ")
+
+if choice == "1":
+    name = input("Enter the name of the game: ").upper()
+    minPlayerCount = int(input("Enter the minimum player count: "))
+    maxPlayerCount = int(input("Enter the maximum player count: "))
+    time = int(input("Enter the amount of minutes the game usually takes: "))
+    age = int(input("Enter the minimum recommended age to play the game: "))
+
+    data = (name, minPlayerCount, maxPlayerCount, time, age, 1)
+    cursor.execute(insert_query, data)
 
 
-data = (name, minPlayerCount, maxPlayerCount, time, age, 1)
-cursor.execute(insert_query, data)
-
-
-#View data in table/columns
-select_query = "SELECT * FROM boardgames;"
-cursor.execute(select_query)
-results = cursor.fetchall()
-for row in results:
-    print(row)
+elif choice == "2":
+    #View data in table/columns
+    select_query = "SELECT * FROM boardgames;"
+    cursor.execute(select_query)
+    columns = [desc[0] for desc in cursor.description]
+    results = cursor.fetchall()
+    print(" | ".join(columns))
+    print("-------------------------------")
+    for row in results:
+        print(" | ".join(map(str,row)))
+        print("-------------------------------")
 
 #example of how to add column
 '''
